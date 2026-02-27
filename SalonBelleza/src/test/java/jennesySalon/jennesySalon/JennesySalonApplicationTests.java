5c11bf112e6dd43b2f124d2250ba225ac53fa027
jennesydua-oss <jennesydua@gmail.com>
2026-02-26 19:22:31 -0600

    Agrego proyecto Spring Boot SalonBelleza

---
+++ SalonBelleza/src/test/java/jennesySalon/jennesySalon/JennesySalonApplicationTests.java
@@ -0,0 +1,13 @@
+package jennesySalon.jennesySalon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JennesySalonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
